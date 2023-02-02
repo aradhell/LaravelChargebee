4867a99deb80b54fe3c4323f5fb0e6f87bba822d
Alper <aradhell@users.noreply.github.com>
2023-02-02 14:05:27 +0100

    Update ChargebeeTransaction.php

--- src/ChargebeeTransaction.php
+++ src/ChargebeeTransaction.php
@@ -6,6 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChargebeeTransaction extends Model
 {
+    /**
+     * TijmenWierenga\LaravelChargebee\ChargebeeTransaction
+     *
+     * @mixin \Eloquent
+     * @property int $id
+     * @property int $customer_id
+     * @property int $amount
+     * @property string $base_currency_code
+     * @property string $currency_code
+     * @property string $payment_date
+     * @property string $gateway
+     * @property string $payment_method
+     * @property string $status
+     * @property string $type
+     * @property bool $deleted
+     * @property float $exchange_rate
+     * @property string $transaction_id
+     * @property string $subscription_id
+     * @property \Illuminate\Support\Carbon|null $created_at
+     * @property \Illuminate\Support\Carbon|null $updated_at
+     */
+
     protected $fillable = [
         'transaction_id',
         'customer_id',
