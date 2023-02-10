c1dc4871eb4544d3b44815ee5781c19654f9db77
Alper <alpercevik@outlook.com>
2023-02-10 09:26:45 +0100

    Update WebhookController.php

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -51,6 +51,8 @@ class WebhookController extends Controller
             $this->handleSubscription($payload);
         } elseif (isset($payload->transaction) && isset($payload->transaction->status)) {
             $this->handlePayment($payload);
+        } elseif (isset($payload->customer) && isset($payload->customer->id)) {
+            $this->handleCustomer($payload);
         }
 
         if (!empty($handler) && method_exists($this, $handler)) {
@@ -139,5 +141,62 @@ class WebhookController extends Controller
         $subscription = (new Cashier::$subscriptionModel)->where('subscription_id', $subscriptionId)->first();
 
         return $subscription;
+    }
+
+    private function handleCustomer($payload)
+    {
+        $customerId = null;
+        if (isset($payload->customer) && isset($payload->customer->id)) {
+            $customerId = $payload->customer->id;
+        }
+
+        $customer = (new Cashier::$subscriptionModel)->where('chargebee_id', $customerId)->firstOrFail();
+
+        if ($payload->customer->first_name) {
+            $customer->first_name = $payload->customer->first_name;
+        }
+        if ($payload->customer->last_name) {
+            $customer->last_name = $payload->customer->last_name;
+        }
+        if ($payload->customer->email) {
+            $customer->email = $payload->customer->email;
+        }
+        if ($payload->customer->company) {
+            $customer->name = $payload->customer->company;
+        }
+        if ($payload->customer->vat_number) {
+            $customer->vat_number = $payload->customer->vat_number;
+            $customer->chargebee_vat_number = $payload->customer->vat_number;
+        }
+        if ($payload->customer->vat_number_status) {
+            $customer->vat_number_status = $payload->customer->vat_number_status;
+        }
+        if ($payload->customer->billing_address->first_name) {
+            $customer->first_name = $payload->customer->billing_address->first_name;
+        }
+        if ($payload->customer->billing_address->last_name) {
+            $customer->last_name = $payload->customer->billing_address->last_name;
+        }
+        if ($payload->customer->billing_address->line1) {
+            $customer->address_line_1 = $payload->customer->billing_address->line1;
+        }
+        if ($payload->customer->billing_address->line2) {
+            $customer->address_line_2 = $payload->customer->billing_address->line2;
+        }
+        if ($payload->customer->billing_address->city) {
+            $customer->city = $payload->customer->billing_address->city;
+        }
+        if ($payload->customer->billing_address->country) {
+            $customer->country_iso2 = $payload->customer->billing_address->country;
+        }
+        if ($payload->customer->billing_address->zip) {
+            $customer->postal_code = $payload->customer->billing_address->zip;
+        }
+        if ($payload->customer->billing_address->validation_status) {
+            $customer->address_validation_status = $payload->customer->billing_address->validation_status;
+        }
+        if ($payload->customer->vat_number_prefix) {
+            $customer->chargebee_vat_number_prefix = $payload->customer->vat_number_prefix;
+        }
     }
 }
