238e2533c20e66347583ccdd879fed45c2b0fe07
Alper <alpercevik@outlook.com>
2023-02-10 10:13:28 +0100

    Update WebhookController.php

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use TijmenWierenga\LaravelChargebee\Cashier;
 use TijmenWierenga\LaravelChargebee\ChargebeeSubscription;
@@ -39,6 +40,8 @@ class WebhookController extends Controller
         'payment_failed' => 'handlePayment',
         'payment_refunded' => 'handlePayment',
         'payment_initiated' => 'handlePayment',
+        'customer_created' => 'handleCustomer',
+        'customer_changed' => 'handleCustomer',
     ];
 
     public function handleWebhook(Request $request)
@@ -47,19 +50,25 @@ class WebhookController extends Controller
         $webhookEvent = Str::studly($request->event_type);
         $payload = json_decode(json_encode($request->input('content')));
 
-        if (isset($payload->subscription) && isset($payload->subscription->status)) {
-            $this->handleSubscription($payload);
-        }
-        if (isset($payload->transaction) && isset($payload->transaction->status)) {
-            $this->handlePayment($payload);
-        }
-        if (isset($payload->customer) && isset($payload->customer->id)) {
-            $this->handleCustomer($payload);
+//        if (isset($payload->subscription) && isset($payload->subscription->status)) {
+//            $this->handleSubscription($payload);
+//        }
+//        if (isset($payload->transaction) && isset($payload->transaction->status)) {
+//            $this->handlePayment($payload);
+//        }
+//        if (isset($payload->customer) && isset($payload->customer->id)) {
+//            $this->handleCustomer($payload);
+//        }
+
+        try {
+            if (!empty($handler) && method_exists($this, $handler)) {
+                $this->{self::$handlers[$request->event_type]}($payload);
+            }
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage());
+            throw $e;
         }
 
-        if (!empty($handler) && method_exists($this, $handler)) {
-            $this->{self::$handlers[$request->event_type]}($payload);
-        }
 
         return response("event " . $webhookEvent, 200);
     }
@@ -70,6 +79,7 @@ class WebhookController extends Controller
      */
     public function handleSubscription($payload)
     {
+        Log::info("handleSubscriotion");
         /** @var ChargebeeSubscription $subscription */
         $subscription = $this->getSubscription($payload->subscription->id);
 
@@ -105,6 +115,7 @@ class WebhookController extends Controller
      */
     public function handlePayment($payload)
     {
+        Log::info("handlePayment");
         $subscriptionId = null;
         if (isset($payload->subscription) && isset($payload->subscription->id)) {
             $subscriptionId = $payload->subscription->id;
@@ -152,53 +163,57 @@ class WebhookController extends Controller
             $customerId = $payload->customer->id;
         }
 
+        Log::info("handleCustomer $customerId");
+
         $customer = (new Cashier::$customerModel)->where('chargebee_id', $customerId)->firstOrFail();
 
-        if ($payload->customer->first_name) {
+        if (isset($payload->customer->first_name)) {
             $customer->first_name = $payload->customer->first_name;
         }
-        if ($payload->customer->last_name) {
+        if (isset($payload->customer->last_name)) {
             $customer->last_name = $payload->customer->last_name;
         }
-        if ($payload->customer->email) {
+        if (isset($payload->customer->email)) {
             $customer->email = $payload->customer->email;
         }
-        if ($payload->customer->company) {
+        if (isset($payload->customer->company)) {
             $customer->name = $payload->customer->company;
         }
-        if ($payload->customer->vat_number) {
+        if (isset($payload->customer->vat_number)) {
             $customer->vat_number = $payload->customer->vat_number;
             $customer->chargebee_vat_number = $payload->customer->vat_number;
         }
-        if ($payload->customer->vat_number_status) {
+        if (isset($payload->customer->vat_number_status)) {
             $customer->vat_number_status = $payload->customer->vat_number_status;
         }
-        if ($payload->customer->billing_address->first_name) {
+        if (isset($payload->customer->billing_address->first_name)) {
             $customer->first_name = $payload->customer->billing_address->first_name;
         }
-        if ($payload->customer->billing_address->last_name) {
+        if (isset($payload->customer->billing_address->last_name)) {
             $customer->last_name = $payload->customer->billing_address->last_name;
         }
-        if ($payload->customer->billing_address->line1) {
+        if (isset($payload->customer->billing_address->line1)) {
             $customer->address_line_1 = $payload->customer->billing_address->line1;
         }
-        if ($payload->customer->billing_address->line2) {
+        if (isset($payload->customer->billing_address->line2)) {
             $customer->address_line_2 = $payload->customer->billing_address->line2;
         }
-        if ($payload->customer->billing_address->city) {
+        if (isset($payload->customer->billing_address->city)) {
             $customer->city = $payload->customer->billing_address->city;
         }
-        if ($payload->customer->billing_address->country) {
+        if (isset($payload->customer->billing_address->country)) {
             $customer->country_iso2 = $payload->customer->billing_address->country;
         }
-        if ($payload->customer->billing_address->zip) {
+        if (isset($payload->customer->billing_address->zip)) {
             $customer->postal_code = $payload->customer->billing_address->zip;
         }
-        if ($payload->customer->billing_address->validation_status) {
+        if (isset($payload->customer->billing_address->validation_status)) {
             $customer->address_validation_status = $payload->customer->billing_address->validation_status;
         }
-        if ($payload->customer->vat_number_prefix) {
+        if (isset($payload->customer->vat_number_prefix)) {
             $customer->chargebee_vat_number_prefix = $payload->customer->vat_number_prefix;
         }
+
+        $customer->save();
     }
 }
